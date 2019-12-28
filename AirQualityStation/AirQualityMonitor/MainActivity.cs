86893e68f6cd6d9d9376a82008009f575e55c1b4
Nanich87 <nanich87@gmail.com>
2019-12-28 22:20:50 +0200

    Aded Solution for Android app

---
+++ AirQualityStation/AirQualityMonitor/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿namespace AirQualityMonitor
+{
+    using Android.App;
+    using Android.OS;
+    using Android.Support.V7.App;
+    using Android.Views;
+
+    [Activity(
+        Label = "@string/app_name",
+        Theme = "@style/AppTheme.NoActionBar",
+        MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (toolbar != null)
+            {
+                SetSupportActionBar(toolbar);
+            }
+        }
+    }
+}
